558faa0bcb9a8eb321797b189d255a0ef77578f0
Koushik Biswas <40529336+kbxkb-games@users.noreply.github.com>
2022-12-11 14:14:48 -0600

    mcvCreateAccount lambda function

---
+++ lambda/mcvCreateAccount.py
@@ -0,0 +1,153 @@
+import os
+import re
+import json
+import boto3
+import logging
+import botocore
+
+#=========================================================
+#GLOBAL VARS
+#=========================================================
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+dynamodb = boto3.resource("dynamodb")
+try:
+    regexUserid = re.compile(r'^[A-Za-z0-9]{6,}$')
+    regexPassword = re.compile(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-\\(\\)]).{8,}$')
+except re.error:
+    logger.error("Invalid regex pattern")
+else:
+    logger.info("Regex compilers initialized!")
+
+#=========================================================
+#Function to validate request body
+#Accepts string, NOT JSON Object
+#Returns Tuple: Boolean (T/F), string message, Json object
+#=========================================================
+def validateJSON(requestBody):
+    if requestBody is not None:
+        requestJsonObject = None
+        try:
+            requestJsonObject = json.loads(requestBody)
+        except ValueError as err:
+            return False, "Request Body ({})is invalid JSON. Error: ".format(requestBody) + " ".join(str(err).split()), None
+        else:
+            if "userid" not in requestJsonObject:
+                return False, "'userid' missing in Request Body", None
+            if not re.fullmatch(regexUserid, requestJsonObject["userid"]):
+                return False, "Value of 'userid' must be alphanumeric with minimum length 6, found {}".format(requestJsonObject["userid"]), None
+            if "password" not in requestJsonObject:
+                return False, "'password' missing in Request Body", None
+            if not re.fullmatch(regexPassword, requestJsonObject["password"]):
+                return False, "Value of 'password' must have 8 characters with at least 1 uppercase, 1 lowercase, 1 number and 1 special character, which can be any of: (.*?[#?!@$%^&*-])", None
+            return True, "", requestJsonObject
+    else:
+        return False, "Request Body cannot be empty. It must be a JSON string specifying \"userid\" and \"password\"", None
+
+def lambda_handler(event, context):
+    #==============================================================
+    #DEFINE BAREBONES RESPONSE, ONE SENT FOR PREFLIGHT OPTIONS
+    #==============================================================
+    #If you get preflight errros on browser, set the following
+    #values explicitly on the CORS pane for the API Gateway:
+    #Access-Control-Allow-Origin:http://localhost:8000 (not *)
+    #Access-Control-Allow-Headers:Content-Type, x-api-key, x-api-id
+    #Access-Control-Allow-Methods:POST, PUT, GET, OPTIONS
+    #Access-Control-Expose-Headers:Content-Type
+    #Access-Control-Max-Age:86400
+    #This should address the issue, and once addressed, revert back
+    #==============================================================
+    responseDictBarebones = {
+        'statusCode': 200,
+        'headers': {
+            #"Access-Control-Allow-Origin": "http://localhost:8000",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST, PUT, GET, OPTIONS",
+            "X-Requested-With": "*",
+            "Access-Control-Allow-Headers": "Content-Type, x-api-key, x-api-id, Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Allow-Methods, X-Requested-With",
+            "Access-Control-Max-Age": 86400
+        }
+    }
+    
+    #=========================================================
+    #VERY BASIC CHECKS HERE...
+    #=========================================================
+    if "httpMethod" not in event or event['httpMethod'] is None:
+        responseDictBarebones['statusCode'] = 400
+        responseDictBarebones['headers']['Access-Control-Expose-Headers'] = "Content-Type"
+        responseDictBarebones['headers']['Content-Type'] = "application/json"
+        responseDictBarebones['body'] = json.dumps('httpMethod is missing in event parameter')
+        return responseDictBarebones
+    if "body" not in event:
+        responseDictBarebones['statusCode'] = 400
+        responseDictBarebones['headers']['Access-Control-Expose-Headers'] = "Content-Type"
+        responseDictBarebones['headers']['Content-Type'] = "application/json"
+        responseDictBarebones['body'] = json.dumps('body is missing in event parameter')
+        return responseDictBarebones
+        
+    #=========================================================
+    #LOGGING
+    #=========================================================
+    logger.info("Request HTTP Method: " + event['httpMethod'])
+    if event['body'] is not None:
+        logger.info("Request Body: " + event['body'])
+    
+    #=========================================================
+    #HANDLE OPTIONS
+    #=========================================================
+    if event['httpMethod'] == 'OPTIONS':
+        return responseDictBarebones
+        
+    #=========================================================
+    #HANDLE ANYTHING OTHER THAN POST
+    #=========================================================
+    if event['httpMethod'] != 'POST':
+        responseDictBarebones['statusCode'] = 400
+        responseDictBarebones['headers']['Access-Control-Expose-Headers'] = "Content-Type"
+        responseDictBarebones['headers']['Content-Type'] = "application/json"
+        responseDictBarebones['body'] = json.dumps('Only POST method allowed for Account Creation')
+        return responseDictBarebones
+        
+    #=========================================================
+    #NOW WE HAVE ONLY POST LEFT
+    #=========================================================
+    else:
+        isRequestBodyValid, errorMessage, requestJsonObject = validateJSON(event['body'])
+        if not isRequestBodyValid:
+            responseDictBarebones['statusCode'] = 400
+            responseDictBarebones['headers']['Access-Control-Expose-Headers'] = "Content-Type"
+            responseDictBarebones['headers']['Content-Type'] = "application/json"
+            responseDictBarebones['body'] = json.dumps(errorMessage)
+            return responseDictBarebones
+        else:
+            userid = requestJsonObject["userid"]
+            password = requestJsonObject["password"]
+            
+            #=========================================================
+            #WRITE TO DYNAMODB TABLE mycatvideos_users
+            #=========================================================
+            try:
+                dynamodb.Table('mycatvideos_users').put_item(
+                    Item={
+                        'userid': userid,
+                        'password': password,
+                        'loggedin': '0'
+                    },
+                    ConditionExpression='attribute_not_exists(userid)'
+                )
+            except botocore.exceptions.ClientError as e:
+                if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
+                    errorMessage = "User id already exists"
+                else:
+                    errorMessage = "Error writing to dynamodb. Details: " + str(e)
+                responseDictBarebones['statusCode'] = 400
+                responseDictBarebones['headers']['Access-Control-Expose-Headers'] = "Content-Type"
+                responseDictBarebones['headers']['Content-Type'] = "application/json"
+                responseDictBarebones['body'] = json.dumps(errorMessage)
+                return responseDictBarebones
+            else:
+                responseBody = "Account created"
+                responseDictBarebones['headers']['Access-Control-Expose-Headers'] = "Content-Type"
+                responseDictBarebones['headers']['Content-Type'] = "application/json"
+                responseDictBarebones['body'] = json.dumps(responseBody)
+                return responseDictBarebones
